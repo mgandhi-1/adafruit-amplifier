c74dbe2330899f03d44d2f416481161c07e27787
mgandhi-1 <mgandhi@snolab.ca>
2025-03-24 14:57:00 -0400

    added code to print ambient temperature and hot junction temperature at 1 second intervals

--- test1.py
+++ test1.py
@@ -2,3 +2,17 @@ import board
 import busio
 from adafruit_bus_device.i2c_device import I2CDevice
 from adafruit_mcp9600 import MCP9600
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=200000)
+
+try:
+	device = MCP9600(i2c)
+	print("Version: ", device.version)
+	while True:
+		print ((device.ambient_temperature, device.temperature))
+		time.sleep(1)
+except ValueError:
+	print("MCP9600 sensor not detected")
+
+
+
